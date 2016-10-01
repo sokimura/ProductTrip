3e747aba89d1aa10a96295682edaf667cd9350ef
kimuraso <kimu.1993416@gmail.com>
2016-10-01 19:48:36 +0900

    update aa

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # sdevise_for :admin_users, ActiveAdmin::Devise.config
-  # ActiveAdmin.routes(self)
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :web
   root :to => 'web#index'
   get 'webs/:id' => 'web#show'
