b781745d563ff8be855860a281eba6a60ff6a36e
kimuraso <kimu.1993416@gmail.com>
2016-10-06 18:57:20 +0900

    add csv8

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+# coding: utf-8
 require "csv"
 
 sites_csv = CSV.readlines("db/sites.csv")
+sites_csv.encode("UTF-8", :invalid => :replace)
 sites_csv.shift
 sites_csv.each do |row|
-  row.encode("UTF-8", :invalid => :replace)
   Site.create(name: row[1], text: row[2], site_url: row[3], iamge_url: row[4], category_id: row[5], created_at: row[6], updated_at: row[7])
 
 
